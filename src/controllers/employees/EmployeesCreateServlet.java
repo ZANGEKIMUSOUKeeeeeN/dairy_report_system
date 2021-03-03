9dac830b471c3e8f489c821a4660ce62cf2145ca
ZANGEKIMUSOUKeeeeeN <df2sh_xxx@yahoo.co.jp>
2021-03-03 21:02:11 +0900

    Add password encryption

--- src/controllers/employees/EmployeesCreateServlet.java
+++ src/controllers/employees/EmployeesCreateServlet.java
@@ -1,6 +1,7 @@
 package controllers.employees;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,20 +14,19 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/employees/create")
 public class EmployeesCreateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public EmployeesCreateServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 	}
 
 }
